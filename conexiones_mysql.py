72d96888525903afa2838ef81ea964887fe9f22f
Rodríguez González <arodriguezgnz@indra.es>
2023-11-19 19:45:12 +0100

    Cambio variable jpool_size de None a 35

---
+++ conexiones_mysql.py
@@ -0,0 +1,44 @@
+"""Script para realizar querys a traves de una pool de MySQL
+"""
+
+from os import getenv
+from mysql.connector import pooling
+
+# Configuración de la conexión a la base de datos MySQL
+db_params = {
+    'host': getenv('host'),
+    'user': getenv('user'),
+    'password': getenv('password'),
+    'database': 'Temp',
+}
+
+# Crear un pool de conexiones sin límite superior
+connection_pool = pooling.MySQLConnectionPool(pool_name="pool",
+                                              pool_size=35,
+                                              **db_params)
+
+def ejecutar_consulta(query):
+    """_summary_
+
+    Args:
+        query (_type_): _description_
+    """
+    connection = connection_pool.get_connection()
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute(query)
+        connection.commit()
+
+    except Exception as e:
+        print(f"Error: {e}")
+        connection.rollback()
+
+    finally:
+        connection.close()
+
+# Ejemplos
+CONSULTA_INSERT = "INSERT INTO tabla (columna1, columna2) VALUES ('valor1', 'valor2')"
+ejecutar_consulta(CONSULTA_INSERT)
+CONSULTA_SELECT = "SELECT * FROM tabla"
+ejecutar_consulta(CONSULTA_SELECT)
