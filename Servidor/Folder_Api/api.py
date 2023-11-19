1f13dea79177592e47ec33b70d6adec321d66d66
Rodríguez González <arodriguezgnz@indra.es>
2023-11-19 19:27:58 +0100

    Creacion fichero datos.xml, para datos poco cambiantes y su despliege en la api

--- Servidor/Folder_Api/api.py
+++ Servidor/Folder_Api/api.py
@@ -4,7 +4,23 @@
     Hecho por: Alejandro Rodríguez González.
 """
 
-from flask import Flask, request, jsonify
+import xml.etree.ElementTree as ET
+from flask import Flask, request
+import conexiones_mysql
+
+
+
+tree = ET.parse('datos.xml')
+root = tree.getroot()
+
+
+maximo_element = root.find('maximo')
+minimo_element = root.find('minimo')
+
+# Obtener los valores máximo y mínimo
+maximo = float(maximo_element.text)
+minimo = float(minimo_element.text)
+
 
 
 # Crear una instancia de la aplicación Flask
@@ -16,11 +32,24 @@ def checker():
     """Check de la conexion al servidor y la version de la Api
 
     Returns:
-        int: 1 --> Version local de la Api 
+        string: 1 --> Version local de la Api 
     """
-    return 1
+    return 'GOOD'
 
+@app.route('/insert', methods=['POST'])
+def insercion():
+    """Método para recivir datos  a traves de un json del ESP32 y enviar a una BBDD. 
 
+    Returns:
+        JSON: GOOD / BAD (strings)
+    """
+    nombre_esp=request.json['esp']
+    dato_temperatura=request.json["temp"]
+    if(dato_temperatura <= maximo)or(dato_temperatura >= minimo):
+        query = f"INSERT INTO Datos (ESP, TEMP ) VALUES ('{nombre_esp}' ,'{dato_temperatura}')"
+        conexiones_mysql.ejecutar_consulta(query)
+        return "GOOD"
+    return "BAD"
 
 
 
